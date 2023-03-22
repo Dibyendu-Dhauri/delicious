ff8bcbb7a0ecffd86fb0cf7b750861d051ba0819
Dibyendu Dhauri <dhauridibyendu@gmail.com>
2023-03-22 10:46:00 +0530

    first commit

---
+++ src/components/Veggie.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide,SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from "react-router-dom";
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+  h3{
+    font-size: 3rem;
+  }
+`;
+
+const Card = styled.div`
+    min-height: 15rem;
+
+    border-radius: 2rem;
+    overflow: hidden;
+   position: relative;
+    img{
+        /* border-radius: 2rem; */
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    p{
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%,0%);
+        color: whitesmoke;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+`;
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+`;
+export default function Popular() {
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+  
+
+        const response = await fetch(
+          `https://api.spoonacular.com/recipes/random?apiKey=${
+            import.meta.env.VITE_API_KEY
+          }&number=9&tags=vegetarian`
+        );
+        const data = await response.json();
+        
+        setVeggie(data.recipes);
+        
+    
+  };
+
+  return (
+    <Wrapper>
+        <h3>Our Vegetarian Picks</h3>
+        <Splide options={{perPage: 4,
+        drag:"free",
+        gap: "5rem"}}>
+      {veggie.map((recipe) => (
+        <SplideSlide key={recipe.id}>
+            <Card >
+               <Link to={"/recipe/" + recipe.id}> 
+          <p>{recipe.title}</p>
+           <img src={recipe.image} alt = {recipe.title}/>
+           <Gradient/>
+           </Link>
+            </Card>
+            </SplideSlide>
+      ))}
+      </Splide>
+    </Wrapper>
+  );
+}
